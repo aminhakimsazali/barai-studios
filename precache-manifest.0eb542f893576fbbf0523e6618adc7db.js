ff9285bea4bd24e89eeed1ec7a0f22bc113b9c10
aminhakimsazali <aminhakimsazali@gmail.com>
2020-08-05 16:06:01 +0800

    Updates

---
+++ precache-manifest.0eb542f893576fbbf0523e6618adc7db.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0d09587643cb2fe74806896e401ee14",
+    "url": "/barai-studios/index.html"
+  },
+  {
+    "revision": "8219c4fa903c968c51d1",
+    "url": "/barai-studios/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "db5c3fce016f6306803b",
+    "url": "/barai-studios/static/css/main.f94d9d2a.chunk.css"
+  },
+  {
+    "revision": "8219c4fa903c968c51d1",
+    "url": "/barai-studios/static/js/2.97f017f7.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/barai-studios/static/js/2.97f017f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db5c3fce016f6306803b",
+    "url": "/barai-studios/static/js/main.499aabbd.chunk.js"
+  },
+  {
+    "revision": "cf1e33d19f2b8a778035",
+    "url": "/barai-studios/static/js/runtime-main.8586f309.js"
+  },
+  {
+    "revision": "e50e8861d8cada34d2e3e675a0ab5790",
+    "url": "/barai-studios/static/media/ashcanbb_bold.e50e8861.ttf"
+  },
+  {
+    "revision": "685914bd2188124ec8ab4be5a460925d",
+    "url": "/barai-studios/static/media/ashcanbb_reg.685914bd.ttf"
+  },
+  {
+    "revision": "58c51129dae8d86bfc0f92bb1be85a7c",
+    "url": "/barai-studios/static/media/icon-insta.58c51129.svg"
+  },
+  {
+    "revision": "427fb06841250707285e5456849a9821",
+    "url": "/barai-studios/static/media/icon-mail.427fb068.svg"
+  },
+  {
+    "revision": "fe42ffdf30fbf543c990428f47c36f6e",
+    "url": "/barai-studios/static/media/icon-phone.fe42ffdf.svg"
+  },
+  {
+    "revision": "279ede6c0224563840910ab3de9ef9aa",
+    "url": "/barai-studios/static/media/icon-ws.279ede6c.svg"
+  },
+  {
+    "revision": "dcadd0f01c54f109101ab309edccc353",
+    "url": "/barai-studios/static/media/logo.dcadd0f0.svg"
+  },
+  {
+    "revision": "e13657ee9975756a551781e49690044b",
+    "url": "/barai-studios/static/media/slide fifth.e13657ee.png"
+  },
+  {
+    "revision": "ce77a6f88d038bed6cfa09fc9618c647",
+    "url": "/barai-studios/static/media/slide six.ce77a6f8.png"
+  },
+  {
+    "revision": "a8fb90a02afd285ce16c5e50713a8f36",
+    "url": "/barai-studios/static/media/slide three.a8fb90a0.png"
+  },
+  {
+    "revision": "d98db31ae10277c8675875c61b03be0c",
+    "url": "/barai-studios/static/media/slide two - 1.d98db31a.png"
+  },
+  {
+    "revision": "704d97d57ecb50d4141d549c7d6fce88",
+    "url": "/barai-studios/static/media/slide two - 2.704d97d5.png"
+  }
+]);
